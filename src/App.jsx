9410a8e5c6a4ef424811f453de32f637a709a30e
vivaldev91 <valtteri.virtanen.dev@gmail.com>
2023-03-01 14:33:00 +0200

    Pet Oweners NoteBook

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState } from 'react'
+import './App.css'
+
+const App = () => {
+
+  return (
+    <div className='App'>
+      <h1>Pet Owner's Notebook</h1>
+    </div>
+  )
+}
+
+
+export default App
