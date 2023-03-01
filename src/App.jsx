d62fecf17c556a41f8a5f9bdc371da3dfe85ad31
vivaldev91 <valtteri.virtanen.dev@gmail.com>
2023-03-01 21:24:39 +0200

    Welcome animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,48 @@
-import React from 'react'
-import { useState } from 'react'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-const App = () => {
+function App() {
+  const [titleOneVisible, setTitleOneVisible] = useState(false);
+  const [titleTwoVisible, setTitleTwoVisible] = useState(false);
+  const [titleThreeVisible, setTitleThreeVisible] = useState(false);
+
+  useEffect(() => {
+    const intervalIdOne = setInterval(() => {
+      setTitleOneVisible(true);
+    }, 500);
+
+    const intervalIdTwo = setInterval(() => {
+      setTitleTwoVisible(true);
+    }, 1500);
+
+    const intervalIdThree = setInterval(() => {
+      setTitleThreeVisible(true);
+    }, 2000);
+
+    return () => {
+      clearInterval(intervalIdOne);
+      clearInterval(intervalIdTwo);
+      clearInterval(intervalIdThree);
+    };
+  }, []);
 
   return (
-    <div className='App'>
-      <h1>Pet Owner's Notebook</h1>
+    <div className="App">
+      <h1 className="title">
+        <span className={`word ${titleOneVisible ? "show" : ""} `}>
+          Welcome
+        </span>
+        <span className={`word ${titleTwoVisible ? "show" : ""}`}>to the</span>
+        <span
+          className={`word ${
+            titleThreeVisible ? "show" : ""
+          } petOwnersNotebook`}
+        >
+          Pet Owner's Notebook
+        </span>
+      </h1>
     </div>
-  )
+  );
 }
 
-
-export default App
+export default App;
