14e115b540cb29f544478f0e24700d6b79be3592
vivaldev91 <valtteri.virtanen.dev@gmail.com>
2023-03-02 18:41:41 +0200

    New start

--- src/App.jsx
+++ src/App.jsx
@@ -2,45 +2,11 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [titleOneVisible, setTitleOneVisible] = useState(false);
-  const [titleTwoVisible, setTitleTwoVisible] = useState(false);
-  const [titleThreeVisible, setTitleThreeVisible] = useState(false);
-
-  useEffect(() => {
-    const intervalIdOne = setInterval(() => {
-      setTitleOneVisible(true);
-    }, 500);
-
-    const intervalIdTwo = setInterval(() => {
-      setTitleTwoVisible(true);
-    }, 1500);
-
-    const intervalIdThree = setInterval(() => {
-      setTitleThreeVisible(true);
-    }, 2000);
-
-    return () => {
-      clearInterval(intervalIdOne);
-      clearInterval(intervalIdTwo);
-      clearInterval(intervalIdThree);
-    };
-  }, []);
+  const [welcome, setWelcome] = useState("Hejssan");
 
   return (
     <div className="App">
-      <h1 className="title">
-        <span className={`word ${titleOneVisible ? "show" : ""} `}>
-          Welcome
-        </span>
-        <span className={`word ${titleTwoVisible ? "show" : ""}`}>to the</span>
-        <span
-          className={`word ${
-            titleThreeVisible ? "show" : ""
-          } petOwnersNotebook`}
-        >
-          Pet Owner's Notebook
-        </span>
-      </h1>
+      <h1>{welcome}</h1>
     </div>
   );
 }
