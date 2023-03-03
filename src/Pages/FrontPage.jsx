21f2936436c05e86ea1ed5b43cd8d7d45526adad
vivaldev91 <valtteri.virtanen.dev@gmail.com>
2023-03-03 02:01:29 +0200

    frontpage styled with input form

--- src/Pages/FrontPage.jsx
+++ src/Pages/FrontPage.jsx
@@ -4,7 +4,13 @@ import "../styles/frontpage.css";
 const FrontPage = () => {
   return (
     <div className="frontpage">
-      <h1 className="forntpageTitle">Pet Owner's Notebook</h1>
+      <h1 className="frontpageTitle">Pet Owner's Notebook</h1>
+      <form className="inputForm">
+        <input type="text" placeholder="Add pet..." className="petInput" />
+        <button type="submit" className="petButton">
+          New pet
+        </button>
+      </form>
     </div>
   );
 };
