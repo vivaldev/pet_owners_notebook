558480a39a20cd93f33e6ac94f0e59ef5920409e
vivaldev91 <valtteri.virtanen.dev@gmail.com>
2023-03-03 02:51:10 +0200

    Scenes start

--- src/Pages/FrontPage.jsx
+++ src/Pages/FrontPage.jsx
@@ -6,6 +6,7 @@ import { useState, useEffect } from "react";
 const FrontPage = () => {
   const [input, setInput] = useState("");
   const [pets, setPets] = useState([]);
+  const [scene, setScene] = useState(1);
 
   useEffect(() => {
     const storedPets = localStorage.getItem("pets");
@@ -26,28 +27,30 @@ const FrontPage = () => {
     e.preventDefault();
     setPets([...pets, input]);
     setInput("");
+    setScene((prevValue) => prevValue + 1);
   }
 
   return (
     <div className="frontpage">
       <h1 className="frontpageTitle">Pet Owner's Notebook</h1>
-      <form className="inputForm" onSubmit={addNewPet}>
-        <input
-          onChange={getInput}
-          type="text"
-          placeholder="Add pet..."
-          className="petInput"
-          value={input}
-        />
-        <button type="submit" className="petButton">
-          New pet
-        </button>
-      </form>
-      <ul>
-        {pets.map((pet, index) => (
-          <li key={index}>{pet}</li>
-        ))}
-      </ul>
+
+      {/* SCENES */}
+      {scene === 1 && (
+        <form className="inputForm" onSubmit={addNewPet}>
+          <input
+            onChange={getInput}
+            type="text"
+            placeholder="Add pet..."
+            className="petInput"
+            value={input}
+          />
+          <button type="submit" className="petButton">
+            New pet
+          </button>
+        </form>
+      )}
+
+      {scene === 2 && <p>Scene 2</p>}
     </div>
   );
 };
